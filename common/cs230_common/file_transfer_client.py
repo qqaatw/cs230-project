53ca95784a6abf88719a4e02a44eb319cf9d00a0
kunwp1 <kunwp1@uci.edu>
2024-02-14 18:29:27 -0800

    Add FileTransferClient class

---
+++ common/cs230_common/file_transfer_client.py
@@ -0,0 +1,128 @@
+import os
+import ftplib
+import socket
+import zipfile
+
+
+class FileTransferClient:
+    def __init__(self, host, port, username, password):
+        if not isinstance(host, str) or not host:
+            raise ValueError("Invalid host")
+        if not isinstance(port, int) or not (0 < port < 65536):
+            raise ValueError("Invalid port")
+        if not isinstance(username, str) or not username:
+            raise ValueError("Invalid username")
+        if not isinstance(password, str) or not password:
+            raise ValueError("Invalid password")
+
+        self.ftp = ftplib.FTP()
+        try:
+            # connect and login to the FTP server
+            self.ftp.connect(host, port)
+            self.ftp.login(username, password)
+            self.ftp.cwd(username)
+        except (socket.error, ftplib.error_perm) as e:
+            print("Error:", e)
+            self.ftp.close()
+            raise e
+        finally:
+            print("Constructor called, FTP connection established.")
+
+    def __del__(self):
+        self.ftp.quit()
+        print("Destructor called, FTP connection closed.")
+
+    """
+    1. Fetch the zip file [task_id]/[task_id].zip from the FTP server
+    2. Create a new folder name [task_id] and unzip the zip file into that folder
+    """
+
+    def fetch_file(self, task_id):
+        if not isinstance(task_id, int) or not task_id:
+            raise ValueError("Invalid task_id")
+
+        zip_filename = str(task_id) + ".zip"
+        ftp_zipfile_path = str(task_id) + "/" + zip_filename
+        local_file = open(zip_filename, "wb")
+
+        try:
+            # retrieve the remote file and write it to the local file
+            self.ftp.retrbinary("RETR " + ftp_zipfile_path, local_file.write)
+        except ftplib.error_perm as e:
+            print("Error:", e)
+            local_file.close()
+            os.remove(zip_filename)
+            raise e
+        finally:
+            local_file.close()
+            print("File fetched successfully:", ftp_zipfile_path)
+
+        folder_name = str(task_id)
+        os.mkdir(folder_name)  # Assume the folder doesn't exist
+
+        try:
+            # open the zip file and extract its contents to the folder
+            with zipfile.ZipFile(zip_filename, "r") as zip_file:
+                zip_file.extractall(folder_name)
+        except zipfile.BadZipFile as e:
+            print("Error:", e)
+            os.rmdir(folder_name)
+            raise e
+        finally:
+            print("File unzipped successfully:", zip_filename)
+
+        os.remove(zip_filename)
+
+    """
+    1. Create a zip file using the file_list
+    2. Create a [task_id] folder in the FTP server and send the zip file to that folder
+    """
+
+    def push_file(self, task_id, file_list):
+        if not isinstance(task_id, int) or not task_id:
+            raise ValueError("Invalid task_id")
+        if not isinstance(file_list, list) or not file_list:
+            raise ValueError("Invalid file_list")
+        for file in file_list:
+            if not isinstance(file, str) or not file:
+                raise ValueError("Invalid file name in file_list")
+
+        zip_filename = str(task_id) + ".zip"
+        zip_file = zipfile.ZipFile(zip_filename, "w")
+
+        try:
+            # add each file from the file_list to the zip file
+            for file in file_list:
+                zip_file.write(file)
+        except (OSError, zipfile.BadZipFile) as e:
+            print("Error:", e)
+            zip_file.close()
+            os.remove(zip_filename)
+            raise e
+        finally:
+            zip_file.close()
+            print("File zipped successfully:", zip_filename)
+
+        remote_file = open(zip_filename, "rb")
+        ftp_zipfile_path = str(task_id) + "/" + zip_filename
+
+        try:
+            # send the zip file to the FTP server
+            self.ftp.mkd(str(task_id))
+            self.ftp.storbinary("STOR " + ftp_zipfile_path, remote_file)
+        except ftplib.error_perm as e:
+            print("Error:", e)
+            remote_file.close()
+            raise e
+        finally:
+            remote_file.close()
+            print("File pushed successfully:", ftp_zipfile_path)
+
+        os.remove(zip_filename)
+        print("File removed successfully:", zip_filename)
+
+    def list_files(self):
+        files = self.ftp.nlst()
+        for file in files:
+            print(file)
+        return files
---
+++ common/examples/file_transfer_client_example.py
@@ -0,0 +1,16 @@
+from cs230_common.file_transfer_client import FileTransferClient
+
+host_ip = "192.168.0.186"
+
+
+def main():
+    FTPServer = FileTransferClient(
+        host=host_ip, port=21, username="kunwp1", password="test"
+    )
+    FTPServer.push_file(1, ["messenger_example.py"])
+    FTPServer.fetch_file(1)
+    FTPServer.list_files()
+
+
+if __name__ == "__main__":
+    main()
