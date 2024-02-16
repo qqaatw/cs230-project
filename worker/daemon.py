17c079cb328d9a44b3f35c7edec20cdec26a2e4d
kunwp1 <143021053+kunwp1@users.noreply.github.com>
2024-02-15 17:37:19 -0800

    Enable Windows users and Anaconda users to create conda env (#2)
    This PR enables Windows users and Anaconda users to create conda env.

--- worker/daemon.py
+++ worker/daemon.py
@@ -21,35 +21,18 @@ class CondaManager:
         self.name = name
         self.path = path
 
-    def check_env_existing(self, delete: bool = True):
-        result = subprocess.run(
-            f"conda env list".split(" "), capture_output=True, text=True, shell=SHELL
-        )
-        for line in result.stdout.split("\n"):
-            if line.split(" ")[0] == self.name:
-                if delete:
-                    subprocess.run(
-                        f"conda env remove -n {self.name} -y".split(" "), shell=SHELL
-                    )
-                    return False
-                return True
-        return False
 
     def build(self):
-        self.check_env_existing(delete=True)
-        mkdir = f"mkdir env".split()
-        if os.path.exists("env"):
+        if os.path.exists('env'):
             raise RuntimeError("Directory `env` already exists.")
-
+    
+        mkdir = f"mkdir env".split()
         print(mkdir)
         result = subprocess.run(mkdir, capture_output=True, text=True, shell=SHELL)
         command = f"conda env create --prefix env -f {self.path}".split(" ")
         print(command)
         result = subprocess.run(command, capture_output=True, text=True, shell=SHELL)
         print(result.stdout, result.stderr)
-
-        if not self.check_env_existing(delete=False):
-            raise RuntimeError("Environment doesn't exist.")
 
 
 """
