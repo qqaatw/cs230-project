26a2d449428a468de758378a869246a07530a57f
kunwp1 <kunwp1@uci.edu>
2024-02-28 16:35:57 -0800

    modify daemon.py:

--- profiler/daemon.py
+++ profiler/daemon.py
@@ -10,28 +10,28 @@ from cs230_common.utils import MessageCategory, MessageBuilder
 def profiler_main(config, messenger):
     while True:
         cpu_percent = psutil.cpu_percent(interval=1)
-        #print(f"CPU Usage: {cpu_percent}%")
         memory_usage = psutil.virtual_memory()
-        #print(memory_usage.total)
-        #print(f"Memory Usage: {memory_usage.percent}%")
-        disk_usage = psutil.disk_usage('/')
-        #print(f"Disk Usage: {disk_usage.percent}%")
+        memory_total = memory_usage.total
+        memory_used = memory_usage.total - memory_usage.available
         gpu_usage = 0
-        
+
         if sys.platform.startswith("win"):
             gpu_stats = gpustat.GPUStatCollection.new_query()
             for gpu in gpu_stats.gpus:
-                #print(f"GPU {gpu.index}: {gpu.name}, Utilization: {gpu.memory_used}")
                 gpu_usage = gpu.memory_used * 1024 * 1024
         else:
-            gpu_usage = torch.mps.current_allocated_memory()
-            
-        msg_body = {"cpu_usage": cpu_percent, "memory_usage": memory_usage.percent, "disk_usage": disk_usage.percent,
+            gpu_usage = memory_used
+        
+        msg_body = {"cpu_usage": cpu_percent, "memory_total": memory_total, "memory_used": memory_used,
                     "gpu_usage": gpu_usage}
+        
+        print(msg_body)
         messenger.produce(
             MessageBuilder.build(MessageCategory.profile, msg_body), "worker_scheduler"
         )
         time.sleep(config["profiler_report_period"])
+
+
 
 with open("../worker/config.json", "r") as f:
     config = json.load(f)
