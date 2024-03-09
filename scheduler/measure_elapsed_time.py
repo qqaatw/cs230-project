ffded137e2ffed46a2b84a754bd4c411e4205d7a
yujeony <eugeneyan@naver.com>
2024-03-09 10:07:32 -0800

    measure elapsed time

---
+++ scheduler/measure_elapsed_time.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+class Elapsed_Time_Tracker:
+    def __init__(self):
+        self.start_time = None
+        self.end_time = None
+
+    def set_start_time(self):
+        if self.start_time is None:
+            self.start_time = datetime.now()
+
+    def update_end_time(self):
+        self.end_time = datetime.now()
+
+    def measure_elapsed_time(self):
+        if self.start_time and self.end_time:
+            return self.end_time - self.start_time
+        else:
+            return None
